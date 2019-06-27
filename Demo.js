4b66f4740d3cbdd47566fda5b07404b360fce96b
Ashwini-Shahapurkar <ashwinishahapurkar28@gmail.com>
2019-06-27 11:25:30 +0530

    3 files modified, 1 added

---
+++ Demo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class Demo extends Component {
+  render() {
+    return (
+      <div className="demo">
+        <h1>hi from external file app.js</h1>
+      </div>
+    );
+  }
+}
+
+export default Demo;
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
+import ReactDOM from 'react-dom';
+import Demo from './Demo.js'; 
 
 class App extends Component {
   constructor() {
@@ -14,13 +16,62 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+        <Hello name={this.state.name} />    
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+//::::::::::::::::::::::::::::::::correct
+//render(<App />, document.getElementById('root'));
+
+//::::::::::::::::::::::::::::::;::correct
+/*function Person(props){
+  
+  return (
+    <div className="person">
+    <h1>{props.firstname}</h1>
+    <p>age :{props.age}</p>
+    </div>
+  );
+  
+}
+
+var app1=(
+  <div>
+  <Person firstname="ashwini" age="22"/>
+  <Person firstname="neha" age="24"/>
+</div>
+)
+
+ReactDOM.render(app1,document.querySelector("#app1"));
+*/
+
+
+//:::::::::::::::::::::::::::::::correct
+/*class Test extends Component{
+  render(){
+  return  <h1>hello world learning react is my dream</h1>
+  }
+}
+ReactDOM.render(<Test />,document.querySelector("#test"));
+*/
+
+//::::::::::::::::::::::::::::correct
+var x=33;
+class Externalfile extends Component{
+  render(){
+    return (
+      <div>
+      {console.log(x)}
+      <Demo />
+      </div>
+    );
+
+}
+}
+ReactDOM.render(<Externalfile />,document.querySelector("#app2"));
+
+//:::::::::::::::::::::::::::::::::correct
+var u=567;
+console.log(u);
